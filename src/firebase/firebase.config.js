bc7bc970c145d5d6a18d46d14f471eaa0ea68a0b
ifat <i>
2024-08-26 17:48:10 +0100

    project setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAglfuIQF6m3ZfLnG0HqqvPD8P18-ADTxM",
+  authDomain: "bbc-developer-portal.firebaseapp.com",
+  projectId: "bbc-developer-portal",
+  storageBucket: "bbc-developer-portal.appspot.com",
+  messagingSenderId: "538241156095",
+  appId: "1:538241156095:web:b3c2b8aae4519d084dddca",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
