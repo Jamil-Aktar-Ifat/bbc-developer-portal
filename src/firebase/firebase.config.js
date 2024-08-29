b6b964b42cb5f722118807ca7d1dc1364062c5e2
ifat <i>
2024-08-29 22:43:40 +0100

    auth redirectedfucntionalities added  after login and firebase config updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log("inside firebase config", import.meta.env.VITE_PASS);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAglfuIQF6m3ZfLnG0HqqvPD8P18-ADTxM",
-  authDomain: "bbc-developer-portal.firebaseapp.com",
-  projectId: "bbc-developer-portal",
-  storageBucket: "bbc-developer-portal.appspot.com",
-  messagingSenderId: "538241156095",
-  appId: "1:538241156095:web:b3c2b8aae4519d084dddca",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
